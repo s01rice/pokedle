600317eedd9588f5057937b5ff97a7866f3777cc
s01rice <s01rice@hotmail.ca>
2022-08-22 05:00:20 -0400

    changed object structure

--- script.js
+++ script.js
@@ -15,40 +15,6 @@ $.ajax({
     })
 });
 
-// today's pokemon
-
-$.ajax({
-    "url": "https://pokeapi.co/api/v2/pokemon/150/",
-    "method": "GET",
-    "timeout": 0,
-}).done(function (pokemon) {
-    // console.log(pokemon);
-    correctGuess["id"] = pokemon.id;
-    correctGuess["name"] = pokemon.species.name;
-    correctGuess["pic"] = pokemon.sprites.front_default;
-    correctGuess["type1"] = pokemon.types[0].type.name;
-    correctGuess["type2"] = pokemon.types.length > 1 ? pokemon.types[1].type.name : "none";
-    correctGuess["weight"] = pokemon.weight;
-
-    $.ajax({
-        "url": pokemon.species.url,
-        "method": "GET",
-        "timeout": 0,
-    }).done(function (species) {
-        // console.log(species);
-        correctGuess["isLegendary"] = species.is_legendary;
-
-        $.ajax({
-            "url": species.evolution_chain.url,
-            "method": "GET",
-            "timeout": 0,
-        }).done(function (evolves) {
-            // console.log(evolves);
-            correctGuess["evolves"] = evolves.chain.evolves_to.length > 0 ? true : false;
-            console.log(correctGuess);
-        });
-    });
-});
 
 // autocomplete for guess box
 
@@ -61,21 +27,74 @@ $(function () {
     })
 });
 
-// guess
 
-$("#guess").submit(function (e) {
-    const guess = $("input").val().charAt(0).toUpperCase() + $("input").val().slice(1);
-    if (pokedex.indexOf(guess) > -1) {
-        alert(guess);
+// today's pokemon
+
+function getPokemon(num) {
+
+    let poke = {};
+
+    $.ajax({
+        "url": "https://pokeapi.co/api/v2/pokemon/" + num + "/",
+        "method": "GET",
+        "timeout": 0,
+    }).done(function (pokemon) {
+        console.log(pokemon);
+        poke["id"] = pokemon.id;
+        poke["name"] = pokemon.species.name;
+        poke["pic"] = pokemon.sprites.front_default;
+        poke["types"] = pokemon.types.map((type) => type.type.name);
+        poke["type"] = pokemon.types.map((type) => type.type.name).join(", ");
+        // poke["type2"] = pokemon.types.length > 1 ? pokemon.types[1].type.name : "none";
+        poke["abilities"] = pokemon.abilities.map((ability) => ability.ability.name);
+        poke["ability"] = pokemon.abilities.map((ability) => ability.ability.name).join(", ");
+        poke["weight"] = pokemon.weight;
+
         $.ajax({
-            "url": "https://pokeapi.co/api/v2/pokemon/" + (pokedex.indexOf(guess) + 1) + "/",
+            "url": pokemon.species.url,
             "method": "GET",
             "timeout": 0,
-        }).done(function (response) {
-            console.log(response);
-            currentGuesses.push(response);
-        });
-    }
+        }).done(function (species) {
+            // console.log(species);
+            poke["isLegendary"] = species.is_legendary;
 
-    e.preventDefault();
+            $.ajax({
+                "url": species.evolution_chain.url,
+                "method": "GET",
+                "timeout": 0,
+            }).done(function (evolves) {
+                console.log(evolves);
+                poke["evolves"] = evolves.chain.evolves_to.length > 0 ? true : false;
+            });
+        });
+    });
+    return poke;
+}
+
+// compare pokemon
+
+function comparePkmn(correctGuess, guess) {
+
+}
+
+// user clicks submit
+$("document").ready(function () {
+
+    correctGuess = getPokemon(3);
+    console.log(correctGuess);
+    var currentGuess;
+
+    $("#guess").submit(function (e) {
+        const guess = $("input").val().charAt(0).toUpperCase() + $("input").val().slice(1);
+        if (pokedex.indexOf(guess) > -1) {
+            console.log(guess);
+            currentGuess = getPokemon(pokedex.indexOf(guess) + 1);
+            console.log(currentGuess);
+
+            // comparePkmn(correctGuess, currentGuess);
+            $("#guess-list").append("<li>a</li>");
+        }
+
+        e.preventDefault();
+    })
 })
